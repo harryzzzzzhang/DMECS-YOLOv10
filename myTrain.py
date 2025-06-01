1c88d7b483f4e2301407a7fe4f2d2a4182970987
Harry <63987959+harryzzzzzhang@users.noreply.github.com>
2025-06-01 21:26:29 +0800

    myTrain.py

--- myTrain.py
+++ myTrain.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLOv10
 import os
 
-model_yaml_path = "./ultralytics/cfg/models/v10/yolov10n_DMECS.yaml" #
-data_yaml_path = './ultralytics/cfg/datasets/a_color_dataset.yaml' # 完全年画
+model_yaml_path = "./ultralytics/cfg/models/v10/yolov10n_DMECS.yaml"
+data_yaml_path = './ultralytics/cfg/datasets/a_color_dataset.yaml'
 
 if __name__ == '__main__':
     model = YOLOv10(model_yaml_path)
