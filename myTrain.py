04f33f83738b7c156921f8031b7a57987b0e6632
Harry <63987959+harryzzzzzhang@users.noreply.github.com>
2025-06-01 21:05:23 +0800

    Add files via upload

---
+++ myTrain.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLOv10
+import os
+
+model_yaml_path = "./ultralytics/cfg/models/v10/yolov10n.yaml" #
+data_yaml_path = './ultralytics/cfg/datasets/a_color_dataset.yaml' # 完全年画
+
+if __name__ == '__main__':
+    model = YOLOv10(model_yaml_path)
+    # model = YOLOv10(model_yaml_path)
+    result = model.train(data=data_yaml_path, epochs=300, batch=20, name="your_test_name")
